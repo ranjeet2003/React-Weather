1d701f7d24d0095312c41505a979e3af6a1ce89f
Ranjeet <ranjeetgautam13032@gmail.com>
2021-04-16 22:23:55 +0530

    connecting app to api call

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import "./App.css";
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+    }
+  };
+
   return (
     <div className="main-container">
       <input
@@ -13,6 +22,7 @@ const App = () => {
         placeholder="search..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
       />
     </div>
   );
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_KEY = "30a27fc813c42027c548f16b2d57f811";
+const API_KEY = "f33a484cf794d08d0148764789aaba32";
 
-const fetchWeather = async (query) => {
+export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
       q: query,
@@ -11,5 +11,6 @@ const fetchWeather = async (query) => {
       APPID: API_KEY,
     },
   });
+
   return data;
 };
